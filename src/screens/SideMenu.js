ccc236c4c5bcb376e85e68437c772550be6a6899
SRoller <sroller@justfab.com>
2019-01-04 12:36:04 -0500

    adds logout button and version text to side menu

--- src/screens/SideMenu.js
+++ src/screens/SideMenu.js
@@ -51,23 +51,29 @@ class SideMenu extends Component {
   render() {
     return (
       <View style={styles.containerStyle}>
-        {/* <FlatList
-          data={this.state.items}
-          extraData={this.state}
-          renderItem={( item ) => (
-            this.renderItem(item.item)
-          )}
-          keyExtractor={ item => item.index.toString() }
-        /> */}
-      <TouchableOpacity style={styles.buttonStyle} onPress={this.navigateToScreen('Home')}>
-        <Text style={this.getStyle('Home')}>Home</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.buttonStyle} onPress={this.navigateToScreen('Profile')}>
-        <Text style={this.getStyle('Profile')}>Profile</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.buttonStyle} onPress={this.navigateToScreen('Settings')}>
-        <Text style={this.getStyle('Settings')}>Settings</Text>
-      </TouchableOpacity>
+          {/* <FlatList
+            data={this.state.items}
+            extraData={this.state}
+            renderItem={( item ) => (
+              this.renderItem(item.item)
+            )}
+            keyExtractor={ item => item.index.toString() }
+          /> */}
+        <TouchableOpacity style={styles.buttonStyle} onPress={this.navigateToScreen('Home')}>
+          <Text style={this.getStyle('Home')}>Home</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.buttonStyle} onPress={this.navigateToScreen('Profile')}>
+          <Text style={this.getStyle('Profile')}>Profile</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.buttonStyle} onPress={this.navigateToScreen('Settings')}>
+          <Text style={this.getStyle('Settings')}>Settings</Text>
+        </TouchableOpacity>
+        <View style={styles.footerContainerStyle}>
+          <TouchableOpacity>
+            <Text style={styles.logoutTextStyle}>Logout</Text>
+          </TouchableOpacity>
+          <Text style={styles.versionTextStyle}>v0.1.0</Text>
+        </View>
       </View>
     );
   }
@@ -95,6 +101,28 @@ const styles = {
     fontSize: 36,
     color: ThemeColor,
     fontWeight: '300'
+  },
+  footerContainerStyle: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    justifyContent: 'space-between',
+    marginLeft: 10,
+    marginRight: 10,
+    marginBottom: 10
+  },
+  logoutTextStyle: {
+    alignSelf: 'center',
+    fontSize: 18,
+    color: ThemeColor,
+    fontWeight: '300'
+  },
+  versionTextStyle: {
+    alignSelf: 'center',
+    fontSize: 18,
+    color: '#4a4a4a',
+    fontWeight: '300',
+    alignSelf: 'flex-end'
   }
 }
 
