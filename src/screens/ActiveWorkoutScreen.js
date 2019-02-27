1a2eeffb97d1e615d15e8855a9c3c9090b438ee3
SRoller <sroller@justfab.com>
2019-02-27 12:43:27 -0500

    adds test library for countdown timer

--- src/screens/ActiveWorkoutScreen.js
+++ src/screens/ActiveWorkoutScreen.js
@@ -1,6 +1,8 @@
 'use strict';
 import React from 'react';
 import { View, Text } from 'react-native';
+import CountdownCircle from 'react-native-countdown-circle';
+import ThemeColor from '../components/Theme';
 
 class ActiveWorkoutScreen extends React.PureComponent {
 
@@ -10,6 +12,15 @@ class ActiveWorkoutScreen extends React.PureComponent {
         <Text>
           This is the active workout screen
         </Text>
+        <CountdownCircle
+          seconds={60}
+          radius={30}
+          borderWidth={8}
+          color={ThemeColor}
+          bgColor='#fff'
+          textStyle={{ fontSize: 20 }}
+          onTimeElapsed={() => console.log('Elapsed!')}
+        />
       </View>
     )
   }
