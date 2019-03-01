efe65600b5116d543d4b8abb8fb04c5bd41e234c
SRoller <sroller@justfab.com>
2019-02-28 22:58:33 -0500

    adds new library for circular progress

--- src/screens/ActiveWorkoutScreen.js
+++ src/screens/ActiveWorkoutScreen.js
@@ -1,7 +1,7 @@
 'use strict';
 import React from 'react';
 import { View, Text } from 'react-native';
-import CountdownCircle from 'react-native-countdown-circle';
+import { AnimatedCircularProgress } from 'react-native-circular-progress';
 import ThemeColor from '../components/Theme';
 
 class ActiveWorkoutScreen extends React.PureComponent {
@@ -12,14 +12,13 @@ class ActiveWorkoutScreen extends React.PureComponent {
         <Text>
           This is the active workout screen
         </Text>
-        <CountdownCircle
-          seconds={60}
-          radius={30}
-          borderWidth={8}
-          color={ThemeColor}
-          bgColor='#fff'
-          textStyle={{ fontSize: 20 }}
-          onTimeElapsed={() => console.log('Elapsed!')}
+        <AnimatedCircularProgress
+          size={120}
+          width={15}
+          fill={100}
+          tintColor="#00e0ff"
+          onAnimationComplete={() => console.log('onAnimationComplete')}
+          backgroundColor="#3d5875" 
         />
       </View>
     )
