091427a0e9fe7f8d6b81b14785d102436fe21e87
SRoller <sroller@justfab.com>
2019-03-01 09:23:23 -0500

    allows control of timer

--- src/screens/ActiveWorkoutScreen.js
+++ src/screens/ActiveWorkoutScreen.js
@@ -1,10 +1,15 @@
 'use strict';
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Easing } from 'react-native';
 import { AnimatedCircularProgress } from 'react-native-circular-progress';
+import { Button } from '../components/common/Button';
 import ThemeColor from '../components/Theme';
 
 class ActiveWorkoutScreen extends React.PureComponent {
+
+  _onButtonPress = () => {
+    this.circularProgress.animate(10, 8000, Easing.quad);
+  }
 
   render() {
     return (
@@ -12,13 +17,15 @@ class ActiveWorkoutScreen extends React.PureComponent {
         <Text>
           This is the active workout screen
         </Text>
+        <Button onPress={this._onButtonPress}>start timer</Button>
         <AnimatedCircularProgress
+          ref={(ref) => this.circularProgress = ref}
+          rotation={-360}
           size={200}
           width={4}
           fill={100}
           tintColor='red'
           onAnimationComplete={() => console.log('onAnimationComplete')}
-          backgroundColor='#fff'
         />
       </View>
     )
