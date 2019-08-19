800dfac18b341a270ac2a7a39edae055b7934b86
SRoller <sroller@justfab.com>
2019-08-18 21:29:39 -0400

    adds typescript support

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import Navigator from './components/navigation/Navigator';
+import { Hello } from './components/tstest/Hello';
+
 
 class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Navigator />
+        <Hello name='Shane' enthusiasmLevel={1} />
       </View>
     );
   }
@@ -15,6 +17,8 @@ class App extends Component {
 const styles = {
   container: {
     flex: 1,
+    alignItems: 'center',
+    alignSelf: 'center',
     backgroundColor: 'white'
   }
 };
