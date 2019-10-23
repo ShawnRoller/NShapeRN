5b211399bef27ab02c05b3caeec752b82f3f3cd2
SRoller <sroller@justfab.com>
2019-10-23 14:43:33 -0400

    adds mock config

---
+++ src/config/__mocks__/firebaseconfig.js
@@ -0,0 +1,10 @@
+const firebaseconfig = {
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: 'nshape-c70dc',
+  storageBucket: 'nshape-c70dc.appspot.com',
+  messagingSenderId: '314558627256'
+}
+
+export { firebaseconfig };
