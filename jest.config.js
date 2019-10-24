2731ee573305995d75cb0d449ea7637559b95541
SRoller <sroller@justfab.com>
2019-10-24 17:41:41 -0400

    adjusts jest config

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,15 @@
+const { defaults } = require('jest-config');
+
 module.exports = {
     preset: 'react-native',
     transform: {
-      '^.+\\.js$': require.resolve('react-native/jest/preprocessor.js'),
-      '^.+\\.tsx?$': 'ts-jest'
+        '^.+\\.js$': require.resolve('react-native/jest/preprocessor.js'),
+        '^.+\\.tsx?$': 'ts-jest'
     },
+    moduleFileExtensions: [
+        'tsx',
+        ...defaults.moduleFileExtensions
+    ],
     setupTestFrameworkScriptFile: "<rootDir>__tests__/setupTests.js",
     transformIgnorePatterns: ["<rootDir>/node_modules/(?!react-native|react-navigation)"]
-  };
+};
